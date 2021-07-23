44a6b3fbd67667a56538b0588cae54eb7d8c68ee
Jean-Philippe Marceau <jpmarceau@coveo.com>
2021-07-23 15:53:40 -0400

    self-review

--- force-app/main/default/aura/customRecommendations/customRecommendationsController.js
+++ force-app/main/default/aura/customRecommendations/customRecommendationsController.js
@@ -8,24 +8,19 @@
               var state = response.getState();
               if (state === 'SUCCESS') {
                 var responseValue = response.getReturnValue();
-                // On doneBuildingQuery, adds the return value of the server-side action to the context.
                 coveoSearchUI.proxyAddEventListener('doneBuildingQuery', function (e, args) {
                   var qb = args.queryBuilder;
                   qb.addContext(responseValue);
                 });
-                // Once we have the user profile name, resolve the pre-init promise.
                 resolve();
               } else if (state === 'ERROR') {
-                // If there was an error, reject the promise.
-                // Handle the error.
                 reject(new Error(response.getError()));
               }
             });
           });
-        // Executes the server-side action.
         $A.enqueueAction(action);
-        // Add the promise to the list of promises the searchUI is waiting on before it initializes.
         coveoSearchUI.addPreInitPromise(getUserProfileNamePromise);
+
         if (recommendationsContext !== '') {
             coveoSearchUI.registerBeforeInit(function (cmp, root, Coveo) {
                 coveoSearchUI.proxyAddEventListener('doneBuildingQuery', function (e, args) {
